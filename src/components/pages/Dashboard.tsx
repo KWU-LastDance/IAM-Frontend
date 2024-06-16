00a3c70a739217c453eaf6731c7880ac11337edd
dhdld <btj5420@naver.com>
2024-06-16 19:13:48 +0900

    add: Dashboard 연결

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import styled from 'styled-components';
+import axios from 'axios';
+
 const Container = styled.div`
     display: flex;
     flex-direction: column;
@@ -11,18 +13,40 @@ const Container = styled.div`
     font-size: 30px;
     font-weight: 700;
     `;
-const Text = styled.p`
-    font-size: 20px;
-    margin-top: 0;
-    margin-bottom: 60px;
-    `;
 
+    const IframeContainer = styled.div`
+    width: 97%;
+    height: 540px;
+    margin-top:10px;
+`;
+
+const StyledIframe = styled.iframe`
+    width: 100%;
+    height: 100%;
+    border: none;
+`;
 
 export function Dashboard() {
+    /*
+    const getDashboard = async () => {
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/dashboard`)
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+    useEffect(() => {
+        getDashboard();
+    }, [])
+    */
     return (
         <Container>
                 <Title>대시보드</Title>
-
+                <IframeContainer>
+                <StyledIframe src="http://localhost:8501" />
+            </IframeContainer>
         </Container>
     )
 }
